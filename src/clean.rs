343f7f5affd9d1a931069564d9ecde79374a6337
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-01 15:13:33 +0900

    Doc

--- src/clean.rs
+++ src/clean.rs
@@ -3,6 +3,13 @@ use std::path::PathBuf;
 use anyhow::Result;
 use clap::Args;
 
+/// Clean unused, old project files.
+///
+/// 1. This runs `git fetch --all --prune` on all projects.
+/// 2. This removes
+///
+///  - the unused files in `target` directory.
+///  - local git brach which has a remote branch but it's removed.
 #[derive(Debug, Args)]
 pub(crate) struct CleanCommand {
     #[clap(short, long)]
