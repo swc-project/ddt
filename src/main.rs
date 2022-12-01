cec313c75bda963d5ba2731d912fefe72359ca04
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-01 15:05:46 +0900

    args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Args {}
+
+#[tokio::main]
+
+async fn main() -> Result<()> {
+    let args = Args::parse();
+
     println!("Hello, world!");
+
+    Ok(())
 }
