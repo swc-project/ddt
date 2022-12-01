ad13c5e09f849b0f21ef186e07db53a3969aaf7f
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-01 15:16:18 +0900

    More

--- src/clean.rs
+++ src/clean.rs
@@ -1,12 +1,12 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use clap::Args;
 
 /// Clean unused, old project files.
 ///
-/// 1. This runs `git fetch --all --prune` on all projects.
-/// 2. This removes
+/// 1. This runs `git fetch --all --prune` on all projects. (does not support
+/// dry run) 2. This removes
 ///
 ///  - the unused files in `target` directory.
 ///  - local git brach which has a remote branch but it's removed.
@@ -21,6 +21,16 @@ pub(crate) struct CleanCommand {
 
 impl CleanCommand {
     pub async fn run(&self) -> Result<()> {
+        let git_projects = find_git_projects(&self.dir)
+            .await
+            .with_context(|| format!("failed to find git projects from {}", self.dir.display()))?;
+
         Ok(())
     }
 }
+
+async fn find_git_projects(dir: &Path) -> Result<Vec<PathBuf>> {
+    Ok(vec![])
+}
+
+async fn run_git_fetch_all_prune(dir: PathBuf) -> Result<()> {}
