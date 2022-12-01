71e475c1dc9b9dbd878c56bef6c75ada77f76ee4
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-01 15:37:15 +0900

    More

--- src/clean.rs
+++ src/clean.rs
@@ -1,7 +1,8 @@
 use std::path::{Path, PathBuf};
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use clap::Args;
+use tokio::process::Command;
 
 /// Clean unused, old project files.
 ///
@@ -36,6 +37,22 @@ async fn find_git_projects(dir: &Path) -> Result<Vec<PathBuf>> {
     Ok(vec![])
 }
 
+/// - `dir`: The root directory of git repository.
 async fn run_git_fetch_all_prune(dir: PathBuf) -> Result<()> {
+    let mut c = Command::new("git");
+    c.arg("fetch").arg("--all").arg("--prune");
+    c.kill_on_drop(true);
+
+    let status = c.status().await.with_context(|| {
+        format!(
+            "failed to get status of `git fetch --all --prune` for {}",
+            dir.display()
+        )
+    })?;
+
+    if !status.success() {
+        bail!("`git fetch --all --prune` failed for {}", dir.display());
+    }
+
     Ok(())
 }
