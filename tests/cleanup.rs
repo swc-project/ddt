afcffa453b905edd2473300cd7892bf675382988
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2023-04-12 16:30:16 -0700

    Wrote function to setup test dir

---
+++ tests/cleanup.rs
@@ -0,0 +1,19 @@
+use anyhow::Result;
+use std::process::Command;
+use tempfile::{tempdir, TempDir};
+
+fn setup_source() -> Result<TempDir> {
+    let dir = tempdir()?;
+    Command::new("cargo")
+        .args(["new", "--lib", "centre"])
+        .current_dir(&dir)
+        .output()?;
+    Ok(dir)
+}
+
+#[test]
+fn cleanup_3_removed_libs() -> Result<()> {
+    let testdir = setup_source()?;
+
+    Ok(())
+}
