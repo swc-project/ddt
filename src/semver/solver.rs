275c7ba5bea90a72754f32269981089e09a8dc35
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-09-06 08:37:25 +0900

    More types

--- src/semver/solver.rs
+++ src/semver/solver.rs
@@ -1,14 +1,22 @@
 use semver::{Version, VersionReq};
+use string_cache::DefaultAtom;
 
 #[async_trait::async_trait]
 pub trait PackageManager {
     async fn resolve(&self, package_name: &str, constraints: &VersionReq) -> Vec<Version>;
 }
 
+pub type PackageName = DefaultAtom;
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Constraints {
+    pub candidate_packages: Vec<PackageName>,
+
     pub compatible_packages: Vec<PackageConstraint>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct PackageConstraint(pub String, pub VersionReq);
+pub struct PackageConstraint {
+    pub name: PackageName,
+    pub constraints: VersionReq,
+}
