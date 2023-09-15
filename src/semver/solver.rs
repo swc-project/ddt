71ad71166ac3de70534aa250882a05ac07984501
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-15 23:35:47 +0900

    Solver

--- src/semver/solver.rs
+++ src/semver/solver.rs
@@ -1,7 +1,13 @@
+use std::sync::Arc;
+
+use anyhow::Result;
+use async_trait::async_trait;
+use auto_impl::auto_impl;
 use semver::{Version, VersionReq};
 use string_cache::DefaultAtom;
 
-#[async_trait::async_trait]
+#[async_trait]
+#[auto_impl(Arc, Box, &)]
 pub trait PackageManager {
     async fn resolve(&self, package_name: &str, constraints: &VersionReq) -> Vec<Version>;
 }
@@ -24,4 +30,33 @@ pub struct Solution {}
 pub struct PackageConstraint {
     pub name: PackageName,
     pub constraints: VersionReq,
+}
+
+#[derive(Debug, Default)]
+struct CargoPackageManager {}
+
+#[async_trait]
+impl PackageManager for CargoPackageManager {
+    async fn resolve(&self, package_name: &str, constraints: &VersionReq) -> Vec<Version> {}
+}
+
+pub async fn solve(
+    constraints: Arc<Constraints>,
+    pkg_mgr: Arc<dyn PackageManager>,
+) -> Result<Solution> {
+    let solver = Solver {
+        constraints,
+        pkg_mgr,
+    };
+
+    solver.solve().await
+}
+
+struct Solver {
+    constraints: Arc<Constraints>,
+    pkg_mgr: Arc<dyn PackageManager>,
+}
+
+impl Solver {
+    async fn solve(&self) -> Result<Solution> {}
 }
