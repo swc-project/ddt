f6432ac64677fc9c55e1a766d78c26a748a51ee5
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-15 22:43:06 +0900

    WIP

--- src/semver/solver.rs
+++ src/semver/solver.rs
@@ -1,4 +1,14 @@
 use auto_impl::auto_impl;
+use gcollections::ops::{Alloc, Empty};
+use interval::{ops::Range, IntervalSet};
+use pcp::{
+    concept::Var,
+    kernel::Snapshot,
+    propagators::{Distinct, XNeqY},
+    search::{one_solution_engine, FDSpace, Status, VStore},
+    term::Addition,
+    variable::Iterable,
+};
 use semver::{Version, VersionReq};
 use string_cache::DefaultAtom;
 
@@ -29,3 +39,60 @@ pub struct PackageConstraint {
 }
 
 pub async fn solve() {}
+
+pub fn nqueens(n: usize) {
+    let mut space = FDSpace::empty();
+
+    let mut queens = vec![];
+    // 2 queens can't share the same line.
+    for _ in 0..n {
+        queens.push(Box::new(space.vstore.alloc(IntervalSet::new(1, n as i32))) as Var<VStore>);
+    }
+    for i in 0..n - 1 {
+        for j in i + 1..n {
+            // 2 queens can't share the same diagonal.
+            let q1 = (i + 1) as i32;
+            let q2 = (j + 1) as i32;
+            // Xi + i != Xj + j reformulated as: Xi != Xj + j - i
+            space.cstore.alloc(Box::new(XNeqY::new(
+                queens[i].bclone(),
+                Box::new(Addition::new(queens[j].bclone(), q2 - q1)) as Var<VStore>,
+            )));
+            // Xi - i != Xj - j reformulated as: Xi != Xj - j + i
+            space.cstore.alloc(Box::new(XNeqY::new(
+                queens[i].bclone(),
+                Box::new(Addition::new(queens[j].bclone(), -q2 + q1)) as Var<VStore>,
+            )));
+        }
+    }
+    // 2 queens can't share the same column.
+    // join_distinct(&mut space.vstore, &mut space.cstore, queens);
+    space.cstore.alloc(Box::new(Distinct::new(queens)));
+
+    // Search step.
+    let mut search = one_solution_engine();
+    search.start(&space);
+    let (frozen_space, status) = search.enter(space);
+    let space = frozen_space.unfreeze();
+
+    // Print result.
+    match status {
+        Status::Satisfiable => {
+            print!(
+                "{}-queens problem is satisfiable. The first solution is:\n[",
+                n
+            );
+            for dom in space.vstore.iter() {
+                // At this stage, dom.lower() == dom.upper().
+                print!("{}, ", dom.lower());
+            }
+            println!("]");
+        }
+        Status::Unsatisfiable => println!("{}-queens problem is unsatisfiable.", n),
+        Status::EndOfSearch => println!("Search terminated or was interrupted."),
+        Status::Unknown(_) => unreachable!(
+            "After the search step, the problem instance should be either satisfiable or \
+             unsatisfiable."
+        ),
+    }
+}
