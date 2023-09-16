9821a6a04218fd1e410673572ca6580b6f9f501f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-16 23:22:01 +0900

    `Intersect`

--- src/util/intersection_union.rs
+++ src/util/intersection_union.rs
@@ -1,17 +1,43 @@
 pub trait Intersect: Sized {
-    fn intersect(self, other: Self) -> Self;
+    type Error;
+
+    fn intersect(self, other: Self) -> Result<Self, Self::Error>;
 }
 
 impl<T> Intersect for Option<T>
 where
     T: Intersect,
 {
-    fn intersect(self, other: Self) -> Self {
-        match (self, other) {
-            (Some(a), Some(b)) => Some(a.intersect(b)),
+    type Error = T::Error;
+
+    fn intersect(self, other: Self) -> Result<Self, Self::Error> {
+        Ok(match (self, other) {
+            (Some(a), Some(b)) => Some(a.intersect(b)?),
             (Some(a), None) => Some(a),
             (None, Some(b)) => Some(b),
             (None, None) => None,
+        })
+    }
+}
+
+mod semver {
+    use semver::{Comparator, Op};
+
+    use super::Intersect;
+
+    impl Intersect for Comparator {
+        type Error = ();
+
+        fn intersect(self, other: Self) -> Result<Self, Self::Error> {
+            if self == other {
+                return Ok(self);
+            }
+
+            match (self.op, other.op) {
+                (Op::Exact, Op::Exact) => Err(()),
+                (Op::Exact, _) => Ok(self),
+                (_, Op::Exact) => Ok(other),
+            }
         }
     }
 }
