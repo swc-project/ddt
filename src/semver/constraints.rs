2cdaf84980a7e599ab61467eca5ff1597267b2ce
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-17 06:40:41 +0900

    `ConstraintStorage`

--- src/semver/constraints.rs
+++ src/semver/constraints.rs
@@ -9,34 +9,22 @@ pub(crate) type ConstraintsPerPkg = AHashMap<PackageName, VersionReq>;
 
 #[derive(Debug)]
 pub(crate) struct ConstraintStorage {
-    actual: ConstraintsPerPkg,
-    parent: Arc<ConstraintStorageParent>,
-}
-
-#[derive(Debug, Default)]
-struct ConstraintStorageParent {
-    cur: Arc<ConstraintsPerPkg>,
-    parent: Option<Arc<ConstraintStorageParent>>,
+    cur: ConstraintsPerPkg,
+    parent: Option<Arc<ConstraintStorage>>,
 }
 
 impl ConstraintStorage {
     pub fn root() -> Self {
         Self {
-            actual: Default::default(),
+            cur: Default::default(),
             parent: Default::default(),
         }
     }
 
-    pub fn new(actual: ConstraintsPerPkg, parent: ConstraintStorage) -> Self {
+    pub fn new(cur: ConstraintsPerPkg, parent: Arc<ConstraintStorage>) -> Self {
         Self {
-            actual: Default::default(),
-            parent: Arc::new(ConstraintStorageParent {
-                cur: Arc::new(actual),
-                parent: Some(Arc::new(ConstraintStorageParent {
-                    cur: Arc::new(parent.actual),
-                    parent: Some(parent.parent),
-                })),
-            }),
+            cur,
+            parent: Some(parent),
         }
     }
 }
