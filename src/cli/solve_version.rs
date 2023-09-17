2ce9c9a1f89c11d2c7b63c6215a78364500c9e6a
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-09-17 13:09:56 +0900

    feat: Implement prototype for the version solver (#12)

--- src/cli/solve_version.rs
+++ src/cli/solve_version.rs
@@ -1,7 +1,15 @@
+use std::sync::Arc;
+
 use anyhow::{Context, Result};
 use clap::Args;
 
-use crate::util::wrap;
+use crate::{
+    semver::{
+        cargo::CargoPackageManager,
+        solver::{solve, Constraints, PackageConstraint},
+    },
+    util::wrap,
+};
 
 #[derive(Debug, Args)]
 pub struct SolveVersionsCommand {}
@@ -10,6 +18,22 @@ impl SolveVersionsCommand {
     pub async fn run(self) -> Result<()> {
         wrap(async move {
             //
+
+            solve(
+                Arc::new(Constraints {
+                    candidate_packages: vec![
+                        "swc_ecma_utils".into(),
+                        "swc_ecma_ast".into(),
+                        "swc_common".into(),
+                    ],
+                    compatible_packages: vec![PackageConstraint {
+                        name: "swc_core".into(),
+                        constraints: "0.79.0".parse().unwrap(),
+                    }],
+                }),
+                Arc::new(CargoPackageManager),
+            )
+            .await?;
 
             Ok(())
         })
