c83de7641cd59aa9e2088c1096e54bc1f0ffff29
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-21 17:15:05 +0900

    script

--- scripts/test-git-auto-resolve.sh
+++ scripts/test-git-auto-resolve.sh
@@ -5,4 +5,4 @@ set -eu
 cargo install --debug --path .
 
 
-(cd lab/git-study && (git merge --abort && true) && git merge test2)
+# (cd lab/git-study && (git merge --abort && true) && git merge test2)
