dcab60cdd871b515a5b9396766d7769ac252e116
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-21 17:58:37 +0900

    `_inner()`

--- src/git.rs
+++ src/git.rs
@@ -8,17 +8,47 @@ pub struct GitWorkflow {}
 /// Methods ported from lint-staged.
 impl GitWorkflow {
     /// Create a diff of partially staged files and backup stash if enabled.
-    pub async fn prepare(&self) -> Result<()> {}
+    #[tracing::instrument(name = "GitWorkflow::prepare", skip_all)]
+    pub async fn prepare(&self) -> Result<()> {
+        self.prepare_inner().await
+    }
+
+    async fn prepare_inner(&self) -> Result<()> {}
 
     /// We need to remove partially staged files, to avoid tasks from seeing
     /// them.
-    pub async fn hide_unstaged_changes(&self, partially_staged_files: Vec<String>) -> Result<()> {}
+    #[tracing::instrument(name = "GitWorkflow::prepare", skip_all)]
+    pub async fn hide_unstaged_changes(&self) -> Result<()> {
+        self.hide_unstaged_changes_inner().await
+    }
 
-    pub async fn apply_modifications(&self, partially_staged_files: Vec<String>) -> Result<()> {}
+    async fn hide_unstaged_changes_inner(&self) -> Result<()> {}
 
-    pub async fn restore_unstaged_changes(&self) -> Result<()> {}
+    #[tracing::instrument(name = "GitWorkflow::prepare", skip_all)]
+    pub async fn apply_modifications(&self) -> Result<()> {
+        self.apply_modifications_inner().await
+    }
 
-    pub async fn restore_original_state(&self) -> Result<()> {}
+    async fn apply_modifications_inner(&self) -> Result<()> {}
 
-    pub async fn cleanup(&self) -> Result<()> {}
+    #[tracing::instrument(name = "GitWorkflow::prepare", skip_all)]
+    pub async fn restore_unstaged_changes(&self) -> Result<()> {
+        self.restore_unstaged_changes_inner().await
+    }
+
+    async fn restore_unstaged_changes_inner(&self) -> Result<()> {}
+
+    #[tracing::instrument(name = "GitWorkflow::prepare", skip_all)]
+    pub async fn restore_original_state(&self) -> Result<()> {
+        self.restore_original_state_inner().await
+    }
+
+    async fn restore_original_state_inner(&self) -> Result<()> {}
+
+    #[tracing::instrument(name = "GitWorkflow::prepare", skip_all)]
+    pub async fn cleanup(&self) -> Result<()> {
+        self.cleanup_inner().await
+    }
+
+    async fn cleanup_inner(&self) -> Result<()> {}
 }
