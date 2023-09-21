e13dcb9de6729f49e2dfff951b54b20873c27f37
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-21 17:54:40 +0900

    doc

--- src/git.rs
+++ src/git.rs
@@ -1,10 +1,15 @@
 //! Utils for interacting with git.
+use anyhow::Result;
 
+/// Utility for git hooks, which cannot use commands like `git add`.
 #[derive(Debug)]
 pub struct GitWorkflow {}
 
 /// Methods ported from lint-staged.
 impl GitWorkflow {
+    /// Create a diff of partially staged files and backup stash if enabled.
+    async fn prepare(&self) -> Result<()> {}
+
     /// We need to remove partially staged files, to avoid tasks from seeing
     /// them.
     pub async fn hide_unstaged_changes(&self, partially_staged_files: Vec<String>) -> Result<()> {}
