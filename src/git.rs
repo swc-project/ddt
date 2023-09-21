1c955b3a2758ed9337907d0d7c25b91a215d3499
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-21 17:52:02 +0900

    GitWorkflow

--- src/git.rs
+++ src/git.rs
@@ -1,1 +1,13 @@
 //! Utils for interacting with git.
+
+#[derive(Debug)]
+pub struct GitWorkflow {}
+
+/// Methods ported from lint-staged.
+impl GitWorkflow {
+    /// We need to remove partially staged files, to avoid tasks from seeing
+    /// them.
+    pub async fn hide_unstaged_changes(&self, partially_staged_files: Vec<String>) -> Result<()> {}
+
+    pub async fn apply_modifications(&self, partially_staged_files: Vec<String>) -> Result<()> {}
+}
