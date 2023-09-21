2da497f9018d674c713f0ec844df04b0582b0361
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-21 16:28:17 +0900

    doc

--- src/cli/git.rs
+++ src/cli/git.rs
@@ -24,13 +24,21 @@ enum Inner {
 }
 
 /// Resolve merge conflicts in the lockfile.
+///
+/// Note that this command do **not** understand the lockfile.
+/// This command simply ignores the conflict and runs some command which can
+/// generate the lockfile.
 #[derive(Debug, Args)]
-struct ResolveLockfileConflictCommand {}
+struct ResolveLockfileConflictCommand {
+    args: Vec<String>,
+}
 
 impl ResolveLockfileConflictCommand {
     pub async fn run(self) -> Result<()> {
         wrap(async move {
             // TODO
+
+            dbg!(&self.args);
 
             bail!("not implemented")
         })
