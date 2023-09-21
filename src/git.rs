c280a2735d36e64ae20d6bcc8c9a8df26df0c28a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-21 18:08:05 +0900

    Move some code

--- src/git.rs
+++ src/git.rs
@@ -1,7 +1,7 @@
 //! Utils for interacting with git.
 use std::sync::Arc;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use tracing::debug;
 
 use crate::util::wrap;
@@ -10,48 +10,65 @@ use crate::util::wrap;
 #[derive(Debug)]
 pub struct GitWorkflow {}
 
+#[derive(Debug, Clone)]
+pub struct PrepareResult {
+    pub partially_staged_files: Arc<Vec<String>>,
+}
+
 /// Methods ported from lint-staged.
 impl GitWorkflow {
     /// Create a diff of partially staged files and backup stash if enabled.
     #[tracing::instrument(name = "GitWorkflow::prepare", skip_all)]
-    pub async fn prepare(self: Arc<Self>) -> Result<()> {
-        self.prepare_inner().await
+    pub async fn prepare(self: Arc<Self>) -> Result<PrepareResult> {
+        wrap(async move { self.prepare_inner().await })
+            .await
+            .context("failed to prepare a git workflow")
     }
 
-    async fn prepare_inner(self: Arc<Self>) -> Result<()> {
-        wrap(async move {
-            debug!("Backing up original state...");
+    async fn prepare_inner(self: Arc<Self>) -> Result<PrepareResult> {
+        debug!("Backing up original state...");
 
-            let partially_staged_files = self.getPartiallyStagedFiles().await?;
+        let partially_staged_files = self.getPartiallyStagedFiles().await?;
 
-            if !partially_staged_files.is_empty() {
-                let unstage_patch = self.getHiddenFilepath(PATCH_UNSTAGED);
-                let files = process_renames(partially_staged_files);
+        if !partially_staged_files.is_empty() {
+            let unstage_patch = self.getHiddenFilepath(PATCH_UNSTAGED);
+            let files = process_renames(partially_staged_files);
 
-                let mut args = vec![String::from("diff")];
-                args.extend(GIT_DIFF_ARGS);
-                args.push("--output".into());
+            let mut args = vec![String::from("diff")];
+            args.extend(GIT_DIFF_ARGS);
+            args.push("--output".into());
 
-                args.push(unstage_patch);
-                args.push("--".into());
-                args.extend(files);
+            args.push(unstage_patch);
+            args.push("--".into());
+            args.extend(files);
 
-                self.exec_git(args).await?;
-            }
+            self.exec_git(args).await?;
+        }
 
-            // TODO: https://github.com/okonet/lint-staged/blob/19a6527c8ac07dbafa2b8c1774e849d3cab635c3/lib/gitWorkflow.js#L210-L229
+        // TODO: https://github.com/okonet/lint-staged/blob/19a6527c8ac07dbafa2b8c1774e849d3cab635c3/lib/gitWorkflow.js#L210-L229
+
+        Ok(PrepareResult {
+            partially_staged_files,
         })
-        .await
     }
 
     /// We need to remove partially staged files, to avoid tasks from seeing
     /// them.
     #[tracing::instrument(name = "GitWorkflow::prepare", skip_all)]
-    pub async fn hide_unstaged_changes(self: Arc<Self>) -> Result<()> {
-        self.hide_unstaged_changes_inner().await
+    pub async fn hide_unstaged_changes(
+        self: Arc<Self>,
+        partially_staged_files: Arc<Vec<String>>,
+    ) -> Result<()> {
+        wrap(async move { self.hide_unstaged_changes_inner().await })
+            .await
+            .context("failed to hide unstaged changes")
     }
 
-    async fn hide_unstaged_changes_inner(self: Arc<Self>) -> Result<()> {}
+    async fn hide_unstaged_changes_inner(
+        self: Arc<Self>,
+        partially_staged_files: Arc<Vec<String>>,
+    ) -> Result<()> {
+    }
 
     #[tracing::instrument(name = "GitWorkflow::prepare", skip_all)]
     pub async fn apply_modifications(self: Arc<Self>) -> Result<()> {
