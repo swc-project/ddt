a289eabf977c97bf242a1c54a2bc5fb2e3944cb4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-21 15:39:03 +0900

    Rfactor

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,7 +1,39 @@
-pub(crate) use self::{
-    clean::CleanCommand, extra::ExtraCommand, solve_version::SolveVersionsCommand,
-};
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+use self::{clean::CleanCommand, extra::ExtraCommand, solve_version::SolveVersionsCommand};
 
 mod clean;
 mod extra;
 mod solve_version;
+
+#[derive(Debug, Parser)]
+pub struct CliArgs {
+    #[clap(subcommand)]
+    cmd: InnerCmd,
+}
+
+impl CliArgs {
+    pub async fn run(self) -> Result<()> {
+        match self.cmd {
+            InnerCmd::Clean(cmd) => {
+                cmd.run().await?;
+            }
+            InnerCmd::SolveVersions(cmd) => {
+                cmd.run().await?;
+            }
+            InnerCmd::X(cmd) => {
+                cmd.run().await?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Subcommand)]
+enum InnerCmd {
+    X(ExtraCommand),
+    Clean(CleanCommand),
+    SolveVersions(SolveVersionsCommand),
+}
