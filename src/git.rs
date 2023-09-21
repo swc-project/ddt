1ac9a7b9d0095ebc32ca78132aa56bd2d6a61458
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-21 17:55:36 +0900

    merhods

--- src/git.rs
+++ src/git.rs
@@ -8,11 +8,17 @@ pub struct GitWorkflow {}
 /// Methods ported from lint-staged.
 impl GitWorkflow {
     /// Create a diff of partially staged files and backup stash if enabled.
-    async fn prepare(&self) -> Result<()> {}
+    pub async fn prepare(&self) -> Result<()> {}
 
     /// We need to remove partially staged files, to avoid tasks from seeing
     /// them.
     pub async fn hide_unstaged_changes(&self, partially_staged_files: Vec<String>) -> Result<()> {}
 
     pub async fn apply_modifications(&self, partially_staged_files: Vec<String>) -> Result<()> {}
+
+    pub async fn restore_unstaged_changes(&self) -> Result<()> {}
+
+    pub async fn restore_original_state(&self) -> Result<()> {}
+
+    pub async fn cleanup(&self) -> Result<()> {}
 }
