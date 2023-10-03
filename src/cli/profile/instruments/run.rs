01569516d2989a15dc7fe29f2625570e928c04d1
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-04 04:06:17 +0900

    more work

--- src/cli/profile/instruments/run.rs
+++ src/cli/profile/instruments/run.rs
@@ -1,20 +1,38 @@
+use std::path::PathBuf;
+
 use anyhow::{bail, Context, Result};
 use clap::Args;
 
-use crate::util::wrap;
+use crate::{
+    cli::profile::instruments::util::{profile_target, CmdArgs, XcodeInstruments},
+    util::wrap,
+};
 
 /// Invoke a binary file under the `instruments` tool.
 #[derive(Debug, Args)]
-pub(super) struct RunCommand {}
+pub(super) struct RunCommand {
+    pub bin: PathBuf,
+
+    pub args: Vec<String>,
+}
 
 impl RunCommand {
     pub async fn run(self) -> Result<()> {
         wrap(async move {
-            // TODO
+            let xctrace_tool = XcodeInstruments::detect().context("failed to detect xctrace")?;
+
+            profile_target(&self.bin, &xctrace_tool, &CmdArgs {})
+                .context("failed to run instruments");
 
             bail!("not implemented")
         })
         .await
-        .context("failed to run instruments with a specified binary")
+        .with_context(|| {
+            format!(
+                "failed to run instruments with `{}` `{:?}",
+                self.bin.display(),
+                self.args
+            )
+        })
     }
 }
