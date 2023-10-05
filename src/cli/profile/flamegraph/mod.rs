aa9a5bb5463910cb5dbe5220c1b1ce97ac7dcb42
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-06 00:22:33 +0900

    Need `CargoCommand`

--- src/cli/profile/flamegraph/mod.rs
+++ src/cli/profile/flamegraph/mod.rs
@@ -1,6 +1,8 @@
+use anyhow::{Context, Result};
 use clap::{Args, Subcommand};
 
 use self::run::RunCommand;
+use crate::util::wrap;
 
 mod cargo;
 mod run;
@@ -9,6 +11,19 @@ mod run;
 pub(super) struct FlamegraphCommand {
     #[clap(subcommand)]
     cmd: Inner,
+}
+
+impl FlamegraphCommand {
+    pub async fn run(self) -> Result<()> {
+        wrap(async move {
+            match self.cmd {
+                Inner::Run(cmd) => cmd.run(Default::default()).await,
+                Inner::Cargo(cmd) => cmd.run().await,
+            }
+        })
+        .await
+        .context("failed to create flamegraph")
+    }
 }
 
 #[derive(Debug, Subcommand)]
