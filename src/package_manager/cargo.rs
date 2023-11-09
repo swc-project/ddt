3eea446e5b84cd409145c2e9dc3e8bbc65a96bcf
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-11-09 13:13:15 +0900

    feat: Use sparse index for `cargo` (#36)

--- src/package_manager/cargo.rs
+++ src/package_manager/cargo.rs
@@ -1,10 +1,11 @@
 use std::sync::Arc;
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use async_trait::async_trait;
-use semver::VersionReq;
+use semver::{Version, VersionReq};
+use serde::Deserialize;
 
-use super::{Dependency, PackageManager, PackageVersion, Versions};
+use super::{Dependency, PackageManager, PackageName, PackageVersion, Versions};
 
 #[derive(Debug, Default)]
 pub struct CargoPackageManager;
@@ -20,43 +21,75 @@ impl PackageManager for CargoPackageManager {
             }]));
         }
 
-        let index = crates_index::GitIndex::new_cargo_default()?;
-        let pkg = index.crate_(package_name).ok_or_else(|| {
-            anyhow!(
-                "Package `{}@{}` not found in index",
-                package_name,
-                constraints
-            )
-        })?;
+        let body = reqwest::get(&build_url(package_name)).await?.text().await?;
 
-        Ok(Arc::new(
-            pkg.versions()
-                .iter()
-                .map(|v| {
-                    let ver = v.version().parse().expect("invalid version");
-
-                    (ver, v.dependencies().to_vec())
-                })
-                .filter(|(v, _)| constraints.matches(v))
-                .map(|(ver, deps)| {
-                    let deps = deps
-                        .iter()
-                        .map(|d| Dependency {
-                            name: d.crate_name().into(),
-                            constraints: d
-                                .requirement()
-                                .parse()
-                                .expect("invalid version requirenment"),
-                        })
-                        .collect();
-
-                    PackageVersion {
-                        name: package_name.into(),
-                        version: ver,
-                        deps,
+        let v = body
+            .lines()
+            .into_iter()
+            .filter_map(|line| {
+                let desc = serde_json::from_str::<Descriptor>(&line);
+                let line = match desc {
+                    Ok(v) => v,
+                    Err(err) => {
+                        return Some(Err(anyhow!("failed to parse line: {:?}\n{}", err, line)))
                     }
-                })
-                .collect(),
-        ))
+                };
+
+                if !constraints.matches(&line.vers) {
+                    return None;
+                }
+
+                Some(Ok(PackageVersion {
+                    name: line.name,
+                    version: line.vers,
+                    deps: line
+                        .deps
+                        .into_iter()
+                        .filter(|dep| dep.kind == "normal")
+                        .map(|d| Dependency {
+                            name: d.package.unwrap_or(d.name),
+                            constraints: d.req,
+                        })
+                        .collect(),
+                }))
+            })
+            .collect::<Result<Vec<_>>>()
+            .with_context(|| format!("failed to parse index of {}", package_name))?;
+
+        Ok(Arc::new(v))
     }
+}
+
+fn build_url(name: &str) -> String {
+    let name = name.to_ascii_lowercase();
+    match name.len() {
+        1 => format!("https://index.crates.io/1/{name}"),
+        2 => format!("https://index.crates.io/2/{name}"),
+        3 => {
+            let first_char = name.chars().next().unwrap();
+            format!("https://index.crates.io/3/{first_char}/{name}")
+        }
+        _ => {
+            let first_two = &name[0..2];
+            let second_two = &name[2..4];
+
+            format!("https://index.crates.io/{first_two}/{second_two}/{name}",)
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct Descriptor {
+    pub name: PackageName,
+    pub vers: Version,
+    pub deps: Vec<DepDescriptor>,
+}
+
+#[derive(Debug, Deserialize)]
+struct DepDescriptor {
+    pub name: PackageName,
+    pub req: VersionReq,
+    pub kind: String,
+    #[serde(default)]
+    pub package: Option<PackageName>,
 }
