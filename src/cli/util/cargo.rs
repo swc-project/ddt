e26ba4de0b87743689839337755e7fe6f5a9dcec
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-05 09:44:24 +0900

    entitlements

--- src/cli/util/cargo.rs
+++ src/cli/util/cargo.rs
@@ -38,7 +38,8 @@ pub async fn get_one_binary_using_cargo(
         let mut cmd = Command::new("codesign");
         cmd.arg("-s").arg("-").arg("-v").arg("-f");
 
-        let entitlements = r#"<?xml version="1.0" encoding="UTF-8"?>
+        let entitlements = r#"
+        <?xml version="1.0" encoding="UTF-8"?>                                                                                                                           ─╯
         <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "https://www.apple.com/DTDs/PropertyList-1.0.dtd"\>
         <plist version="1.0">
             <dict>
