79a60d7e8b95f5733455c9c33e7f297e725d9db3
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-18 15:19:07 +0900

    document

--- src/cli/cargo/bin_size/mod.rs
+++ src/cli/cargo/bin_size/mod.rs
@@ -1,11 +1,24 @@
 use anyhow::Result;
-use clap::Args;
+use clap::{Args, Subcommand};
 
+/// Comamnds to reduce the size of the binary.
 #[derive(Debug, Args)]
-pub(super) struct BinSizeCommand {}
+pub(super) struct BinSizeCommand {
+    #[clap(subcommand)]
+    cmd: Cmd,
+}
 
 impl BinSizeCommand {
     pub async fn run(self) -> Result<()> {
         Ok(())
     }
 }
+
+#[derive(Debug, Subcommand)]
+enum Cmd {
+    SelectPerCrate(SelectPerCrateCommand),
+}
+
+/// Select the optimization level for each crate.
+#[derive(Debug, Args)]
+struct SelectPerCrateCommand {}
